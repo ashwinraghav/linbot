fb90d0c9a2ced208ae02d93294e18fc5863e533a
Your Name <armohan@thoughtworks.com>
2011-02-17 15:24:13 +0530

    move location to the third column

--- spreadsheet_service.rb
+++ spreadsheet_service.rb
@@ -7,7 +7,8 @@ class SpreadsheetService
 
     print_entity[:members].each_with_index do |member, i|
       sheet[i, 0] = member[:name]
-      sheet[i, 1] = member[:profile]
+      sheet[i,1] = member[:profile].split(",")[0..-2].join(",")
+      sheet[i,2] = member[:profile].split(",").last
     end
 
     book.write FILE_NAME
